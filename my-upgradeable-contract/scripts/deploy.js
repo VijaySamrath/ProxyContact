cefca4fd73c6bef66566e92957d4dcb8bca63648
vijay samrath <vijaysamrath462@gmail.com>
2023-07-20 21:28:45 +0530

    update commit

--- my-upgradeable-contract/scripts/deploy.js
+++ my-upgradeable-contract/scripts/deploy.js
@@ -1,8 +1,8 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const Storage = await ethers.getContractFactory("Storage");
-  const proxy = await upgrades.deployProxy(Storage, [12]);
+  const BoxV1 = await ethers.getContractFactory("BoxV1");
+  const proxy = await upgrades.deployProxy(BoxV1, [10, 2],{initializer:'initialize'});
   await proxy.deployed();
 
   console.log(proxy.address);
